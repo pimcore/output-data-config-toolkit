cc0299046ad04881a8bc117ed3d1e0a5db072cf9
MSchweiger <michael.schweiger@element.at>
2015-11-09 13:48:03 +0100

    TranslateValue added

---
+++ lib/Elements/OutputDataConfigToolkit/ConfigElement/Operator/TranslateValue.php
@@ -0,0 +1,45 @@
+<?php
+namespace Elements\OutputDataConfigToolkit\ConfigElement\Operator;
+ 
+class TranslateValue extends AbstractOperator {
+
+    private $prefix;
+
+    public function __construct($config, $context = null) {
+        parent::__construct($config, $context);
+
+        $this->prefix = $config->prefix;
+    }
+
+    public function getLabeledValue(\Object_Abstract $object) {
+        $childs = $this->getChilds();
+        if($childs[0]) {
+            $translate = new \Pimcore_Translate_Website(\Zend_Registry::get('Zend_Locale'));
+
+            $value = $childs[0]->getLabeledValue($object);
+            $value->value = $translate->translate($this->prefix . $value->value);
+
+            return $value;
+        }
+        return null;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrefix()
+    {
+        return $this->prefix;
+    }
+
+    /**
+     * @param mixed $prefix
+     */
+    public function setPrefix($prefix)
+    {
+        $this->prefix = $prefix;
+    }
+
+
+
+}
