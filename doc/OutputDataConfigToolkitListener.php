e16bca6f6c0b2422d92edf618280c84d40dee760
Alex Zamponi <562324+alexz707@users.noreply.github.com>
2025-04-09 13:34:24 +0200

    Refactor methods to include return types to remove deprecations

--- doc/OutputDataConfigToolkitListener.php
+++ doc/OutputDataConfigToolkitListener.php
@@ -28,7 +28,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class OutputDataConfigToolkitListener implements EventSubscriberInterface
 {
 
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             OutputDataConfigToolkitEvents::INITIALIZE => "onInitialize"
