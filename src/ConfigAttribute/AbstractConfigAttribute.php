357429cad1c43193194ebbbbfd59868c0fdf8a9a
Christian Fasching <christian.fasching@pimcore.com>
2025-04-30 06:51:25 +0200

    License Switch to POCL

--- src/ConfigAttribute/AbstractConfigAttribute.php
+++ src/ConfigAttribute/AbstractConfigAttribute.php
@@ -1,16 +1,13 @@
 <?php
 
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
  */
 
 namespace OutputDataConfigToolkitBundle\ConfigAttribute;
@@ -22,16 +19,16 @@ namespace OutputDataConfigToolkitBundle\ConfigAttribute;
  */
 abstract class AbstractConfigAttribute implements IConfigAttribute
 {
-    /* @var string|null $label */
+    // @var string|null $label
     protected $label;
 
-    /* @var string|null $type */
+    // @var string|null $type
     protected $type;
 
-    /* @var string|null $class */
+    // @var string|null $class
     protected $class;
 
-    /* @var AbstractConfigAttribute[] $childs */
+    // @var AbstractConfigAttribute[] $childs
     protected $childs;
 
     abstract public function applyDefaults();
--- src/ConfigAttribute/Operator/Concatenator.php
+++ src/ConfigAttribute/Operator/Concatenator.php
@@ -1,16 +1,13 @@
 <?php
 
 /**
- * Pimcore
- *
- * This source file is available under two different licenses:
- * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Commercial License (PCL)
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
  */
 
 namespace OutputDataConfigToolkitBundle\ConfigAttribute\Operator;
@@ -21,13 +18,13 @@ use OutputDataConfigToolkitBundle\Tools\Util;
 
 class Concatenator extends AbstractConfigAttributeOperator
 {
-    /* @var string|null $glue */
+    // @var string|null $glue
     private $glue;
 
-    /* @var bool $forceValue */
+    // @var bool $forceValue
     private $forceValue;
 
-    /* @var bool $formatNumbers */
+    // @var bool $formatNumbers
     private $formatNumbers;
 
     /**
